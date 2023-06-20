bb7ccbe3890b00f0f56a335951b15d7f0987f078
Kami1l <kamil.kozik44@gmail.com>
2023-06-20 13:32:52 +0200

    Added comments and make code more transparent

--- main.py
+++ main.py
@@ -10,10 +10,16 @@ newFile = open("data.json","w")
 Tk().withdraw() 
 fileName = askopenfilename() 
 
+# Open a excel file
+
 workbook = load_workbook(filename=fileName,read_only=True)
 sheet = workbook.active
 
+# Create dictionary
+
 objects = {}
+
+# Get data from excel file
 
 for row in sheet.iter_rows(min_row=1,values_only=True):
 
@@ -25,10 +31,13 @@ for row in sheet.iter_rows(min_row=1,values_only=True):
         "Age":row[4],
         "Climat":row[5]
     }
+
+    # Add data to dictionary
+
     objects[object_name] = object
-    
-    print(json.dumps(objects))
+    # print(json.dumps(objects))
+
+# Write object to json file 
 
 newFile.write(json.dumps(objects))
-
 newFile.close()
